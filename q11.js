9c26db67d044a0ec7eeefc6dd6eefd0b053ac46a
SreelakshmiMithra <lakshmi.badiginchala23@gmail.com>
2025-11-13 23:08:03 +0530

    step 1 is done

---
+++ q11.js
@@ -0,0 +1,16 @@
+let num = 15;
+function checkPrime(num){
+    let count = 0
+    for(let i =1;i<=num;i++){
+        if(num%i==0){
+            count++
+        }
+    }
+    if(count==2){
+        return true
+    }else{
+        return false
+    }
+}
+
+console.log(checkPrime(num))
